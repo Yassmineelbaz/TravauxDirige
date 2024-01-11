5b04fbb234d1fb390bdd636475d60df81d3db3f6
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:40:55 +0100

    Centre.java

--- src/lst/travauxdirige/serie3/Centre.java
+++ src/lst/travauxdirige/serie3/Centre.java
@@ -3,18 +3,18 @@ package lst.travauxdirige.serie3;
 class Centre extends Point {
     private char nom;
 
-    // Constructeur de la classe Centre
+   
     public Centre(int x, int y, char nom) {
         super(x, y);
         this.nom = nom;
     }
 
-    // Méthode pour afficher le nom d'un objet de type Centre
+   
     public void affNom() {
         System.out.println("Nom : " + nom);
     }
 
-    // Méthode pour afficher les coordonnées d'un objet de type Centre
+   
     @Override
     public void affCoord() {
         super.affCoord();  // Appelle la méthode affCoord de la classe Point
