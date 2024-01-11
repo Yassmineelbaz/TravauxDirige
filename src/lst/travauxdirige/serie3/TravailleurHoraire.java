0a7dff24b71b4b88c689dfd0df708b8462df6147
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:00:40 +0100

    TravailleurHoraire.java

--- src/lst/travauxdirige/serie3/TravailleurHoraire.java
+++ src/lst/travauxdirige/serie3/TravailleurHoraire.java
@@ -4,7 +4,6 @@ class TravailleurHoraire extends Employe {
     private double retribution;
     private double heures;
 
-    // Constructeurs de la classe TravailleurHoraire
     public TravailleurHoraire() {
         super();
         this.retribution = 0.0;
@@ -17,7 +16,6 @@ class TravailleurHoraire extends Employe {
         this.heures = 0.0;
     }
 
-    // Accesseurs et mutateurs pour les attributs de TravailleurHoraire
     public double getRetribution() {
         return retribution;
     }
@@ -34,13 +32,11 @@ class TravailleurHoraire extends Employe {
         this.heures = heures;
     }
 
-    // Implémentation de la méthode gains pour le salaire du travailleur horaire
     @Override
     public double gains() {
         return retribution * heures;
     }
 
-    // Méthode toString pour afficher une représentation du travailleur horaire
     @Override
     public String toString() {
         return super.toString() + ", Rémunération horaire : " + retribution + ", Heures : " + heures;
