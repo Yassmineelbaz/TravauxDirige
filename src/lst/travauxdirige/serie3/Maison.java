0a69b026903d99c080689c40bb4ed5dd56949de8
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:57:14 +0100

    Maison.java

--- src/lst/travauxdirige/serie3/Maison.java
+++ src/lst/travauxdirige/serie3/Maison.java
@@ -3,7 +3,6 @@ package lst.travauxdirige.serie3;
 class Maison extends Batiment {
     private int nbChambres;
 
-    // Constructeurs de la classe Maison
     public Maison() {
         super();
         this.nbChambres = 0;
@@ -14,7 +13,6 @@ class Maison extends Batiment {
         this.nbChambres = nbChambres;
     }
 
-    // Accesseurs et mutateurs pour l'attribut nbChambres
     public int getNbChambres() {
         return nbChambres;
     }
@@ -23,7 +21,6 @@ class Maison extends Batiment {
         this.nbChambres = nbChambres;
     }
 
-    // Méthode toString pour afficher une représentation de la Maison
     @Override
     public String toString() {
         return super.toString() + ", Maison - Nb Chambres : " + nbChambres;
