fb4f646f4b8c8f9ad75b708bfd327e201735e3d0
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:11:46 +0100

    CompteurEtudiant.java

--- src/lst/travauxdirige/serie1/CompteurEtudiant.java
+++ src/lst/travauxdirige/serie1/CompteurEtudiant.java
@@ -5,27 +5,21 @@ public class CompteurEtudiant {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            // Demander à l'utilisateur de saisir le nombre d'étudiants
             System.out.print("Entrez le nombre d'étudiants : ");
             int nombreEtudiants = scanner.nextInt();
 
-            // Créer le tableau des moyennes
             double[] moyennes = new double[nombreEtudiants];
 
-            // Saisir les moyennes des étudiants
             for (int i = 0; i < nombreEtudiants; i++) {
                 System.out.print("Entrez la moyenne de l'étudiant " + (i + 1) + " : ");
                 moyennes[i] = scanner.nextDouble();
             }
 
-            // Demander à l'utilisateur de saisir le seuil
             System.out.print("Entrez le seuil de moyenne : ");
             double seuil = scanner.nextDouble();
 
-            // Compter le nombre d'étudiants ayant une moyenne supérieure ou égale au seuil
             int nombreEtudiantsSupSeuil = compterEtudiantsSupSeuil(moyennes, seuil);
 
-            // Afficher le résultat
             System.out.println("Nombre d'étudiants ayant une moyenne supérieure ou égale au seuil : " + nombreEtudiantsSupSeuil);
 
             scanner.close();
