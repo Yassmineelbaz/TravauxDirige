b43f1e9cbc0379edfc16c7751aeab49a78487e16
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:15:33 +0100

    Fibo.java

--- src/lst/travauxdirige/serie1/Fibo.java
+++ src/lst/travauxdirige/serie1/Fibo.java
@@ -7,15 +7,12 @@ public class Fibo {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            // Demander à l'utilisateur de saisir un entier positif n
             System.out.print("Entrez un entier positif n : ");
             int n = scanner.nextInt();
 
-            // Vérifier si n est un entier positif
             if (n < 0) {
                 System.out.println("Veuillez entrer un entier positif.");
             } else {
-                // Calculer et afficher le nème terme de la suite de Fibonacci
                 long result = calculerFibonacci(n);
                 System.out.println("Le " + n + "ème terme de la suite de Fibonacci est : " + result);
             }
@@ -23,7 +20,6 @@ public class Fibo {
             scanner.close();
         }
 
-        // Méthode pour calculer le nème terme de la suite de Fibonacci de manière itérative
         private static long calculerFibonacci(int n) {
             if (n <= 1) {
                 return n;
