11a1e192f19272c4c2a99da102b24a32bfccf74e
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:59:16 +0100

    TestEmploye.java

--- src/lst/travauxdirige/serie3/TestEmploye.java
+++ src/lst/travauxdirige/serie3/TestEmploye.java
@@ -3,20 +3,17 @@ package lst.travauxdirige.serie3;
 
     public class TestEmploye {
         public static void main(String[] args) {
-            // Test de la classe Patron
             Patron patron = new Patron("Dupont", "Jean", 5000.0);
 
             System.out.println(patron);
             System.out.println("Salaire du patron : " + patron.gains());
 
-            // Test de la classe TravailleurCommission
             TravailleurCommission travailleurCommission = new TravailleurCommission("Martin", "Alice", 1000.0, 0.05);
             travailleurCommission.setQuantite(200);
 
             System.out.println("\n" + travailleurCommission);
             System.out.println("Salaire du travailleur à la commission : " + travailleurCommission.gains());
 
-            // Test de la classe TravailleurHoraire
             TravailleurHoraire travailleurHoraire = new TravailleurHoraire("Dubois", "Paul", 15.0);
             travailleurHoraire.setHeures(40);
 
