a08773e674d165717b1c6bc41c8afc74f0d2d650
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:00:57 +0100

    Triporteur.java

--- src/lst/travauxdirige/serie3/Triporteur.java
+++ src/lst/travauxdirige/serie3/Triporteur.java
@@ -3,7 +3,6 @@ package lst.travauxdirige.serie3;
 class Triporteur extends Moto {
     private int nbTriporteur;
 
-    // Constructeur Triporteur avec deux paramètres
     public Triporteur(int nbP, int nbT) {
         super(nbP);
         this.nbTriporteur = nbT;
