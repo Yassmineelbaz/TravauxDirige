7370ef39ab3806e3c2e422f081b0087518b4e216
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:59:45 +0100

    TestFigure.java

--- src/lst/travauxdirige/serie3/TestFigure.java
+++ src/lst/travauxdirige/serie3/TestFigure.java
@@ -2,7 +2,6 @@ package lst.travauxdirige.serie3;
 
 public class TestFigure {
     public static void main(String[] args) {
-        // Test de la classe Disque
         Point centreDisque = new Point(2, 3);
         Disque disque = new Disque(centreDisque, 5.0, "Grand Disque");
 
@@ -10,7 +9,6 @@ public class TestFigure {
         System.out.println("Périmètre : " + disque.getPerimeter());
         System.out.println("Aire : " + disque.getAire());
 
-        // Test de la classe Rectangle
         Rectangle rectangle = new Rectangle(4.0, 6.0, "Grand Rectangle");
 
         System.out.println("\n" + rectangle);
