a6ab9c48e59809e4ae901a95cacbf0ba3fcf5b51
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:23:06 +0100

    PermutationEntier.java

--- src/lst/travauxdirige/serie1/PermutationEntier.java
+++ src/lst/travauxdirige/serie1/PermutationEntier.java
@@ -4,27 +4,21 @@ import java.util.Scanner;
 
 public class PermutationEntier {
     public static void main(String[] args) {
-        // Créer un objet Scanner pour lire les entrées au clavier
         Scanner scanner = new Scanner(System.in);
 
-        // Demander à l'utilisateur d'entrer un entier de trois chiffres non nuls
         System.out.print("Entrez un entier de trois chiffres non nuls : ");
         int N = scanner.nextInt();
 
-        // Fermer le scanner pour éviter les fuites de ressources
         scanner.close();
 
-        // Vérifier si l'entier a trois chiffres non nuls
         if (N < 100 || N > 999 || N % 111 == 0) {
             System.out.println("Veuillez entrer un entier de trois chiffres non nuls.");
         } else {
-            // Déterminer et afficher toutes les permutations des chiffres de N
             System.out.println("Les permutations possibles sont :");
             afficherPermutations(N);
         }
     }
 
-    // Méthode pour afficher toutes les permutations des chiffres de l'entier
     private static void afficherPermutations(int N) {
         int centaine = N / 100;
         int dizaine = (N % 100) / 10;
