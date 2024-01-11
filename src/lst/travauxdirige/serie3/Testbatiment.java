fc788f4403d1bcecdbe891a66ae844b1ab19937d
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:00:09 +0100

    Testbatiment.java

--- src/lst/travauxdirige/serie3/Testbatiment.java
+++ src/lst/travauxdirige/serie3/Testbatiment.java
@@ -2,21 +2,18 @@ package lst.travauxdirige.serie3;
 
 public class Testbatiment {
     public static void main(String[] args) {
-        // Test de la classe Batiment
         Batiment batiment1 = new Batiment();
         Batiment batiment2 = new Batiment("123 Rue de la Ville");
 
         System.out.println("Batiment1 : " + batiment1);
         System.out.println("Batiment2 : " + batiment2);
 
-        // Test de la classe Maison
         Maison maison1 = new Maison();
         Maison maison2 = new Maison("456 Avenue du Quartier", 3);
 
         System.out.println("\nMaison1 : " + maison1);
         System.out.println("Maison2 : " + maison2);
 
-        // Test de la classe Immeuble
         Immeuble immeuble1 = new Immeuble();
         Immeuble immeuble2 = new Immeuble("789 Boulevard de la Ville", 10);
 
