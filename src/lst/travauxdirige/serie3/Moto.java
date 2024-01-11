306dc398549538713eea14ed7cd8b7b8eb4f16ca
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:57:30 +0100

    Moto.java

--- src/lst/travauxdirige/serie3/Moto.java
+++ src/lst/travauxdirige/serie3/Moto.java
@@ -4,7 +4,6 @@ package lst.travauxdirige.serie3;
 class Moto extends Vehicule {
     private int nbRoues = 2;
 
-    // Constructeur de la classe Moto
     public Moto(int nbPassager) {
         super(nbPassager); // Appel du constructeur de la classe Vehicule
     }
