3ad17f4dad9a5488b3ae9b334bc121714a8eb255
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:23:50 +0100

    Produit.java

--- src/lst/travauxdirige/serie1/Produit.java
+++ src/lst/travauxdirige/serie1/Produit.java
@@ -2,20 +2,16 @@ package lst.travauxdirige.serie1;
 import java.util.Scanner;
 public class Produit {
     public static void main(String[] args) {
-        // Créer un objet Scanner pour lire les entrées au clavier
         Scanner scanner = new Scanner(System.in);
 
-        // Demander à l'utilisateur d'entrer deux valeurs entières
         System.out.print("Entrez la valeur de A : ");
         int A = scanner.nextInt();
 
         System.out.print("Entrez la valeur de B : ");
         int B = scanner.nextInt();
 
-        // Fermer le scanner pour éviter les fuites de ressources
-        scanner.close();
+        
 
-        // Déterminer le signe du produit sans faire la multiplication
         int produit = A * B;
         String signeProduit;
 
@@ -27,7 +23,6 @@ public class Produit {
             signeProduit = "nul";
         }
 
-        // Afficher le signe du produit
         System.out.println("Le signe du produit de A et B est " + signeProduit + ".");
     }
 }
