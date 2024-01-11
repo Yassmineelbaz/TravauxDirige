f03203ed9e4610cda5804e10901253385658799a
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:01:13 +0100

    Vehicule.java

--- src/lst/travauxdirige/serie3/Vehicule.java
+++ src/lst/travauxdirige/serie3/Vehicule.java
@@ -3,12 +3,10 @@ package lst.travauxdirige.serie3;
 public class Vehicule {
     private int nbPassager;
 
-    // Constructeur de la classe Vehicule
     public Vehicule(int nbPassager) {
         this.nbPassager = nbPassager;
     }
 
-    // Méthode toString pour afficher le nombre de passagers
     @Override
     public String toString() {
         return "Nombre de passagers : " + nbPassager;
