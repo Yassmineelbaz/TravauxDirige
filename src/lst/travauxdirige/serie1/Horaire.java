cef387df8176562cc18c74c47a4828e1973b7ca9
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:16:14 +0100

    Horaire.java

--- src/lst/travauxdirige/serie1/Horaire.java
+++ src/lst/travauxdirige/serie1/Horaire.java
@@ -2,21 +2,16 @@ package lst.travauxdirige.serie1;
 import java.util.Scanner;
 public class Horaire {
     public static void main(String[] args) {
-        // Créer un objet Scanner pour lire les entrées au clavier
         Scanner scanner = new Scanner(System.in);
 
-        // Demander à l'utilisateur d'entrer l'heure
         System.out.print("Entrez l'heure (format 24 heures) : ");
         int heure = scanner.nextInt();
 
-        // Demander à l'utilisateur de choisir la langue
         System.out.print("Choisissez la langue ('f' pour français, 'a' pour anglais) : ");
         char choixLangue = scanner.next().charAt(0);
 
-        // Fermer le scanner pour éviter les fuites de ressources
         scanner.close();
 
-        // Afficher le message approprié en fonction de l'heure et de la langue
         if (choixLangue == 'f') {
             afficherMessageFrancais(heure);
         } else if (choixLangue == 'a') {
@@ -26,7 +21,6 @@ public class Horaire {
         }
     }
 
-    // Méthode pour afficher le message en français en fonction de l'heure
     private static void afficherMessageFrancais(int heure) {
         if (heure >= 0 && heure < 18) {
             System.out.println("Bonjour");
@@ -37,7 +31,6 @@ public class Horaire {
         }
     }
 
-    // Méthode pour afficher le message en anglais en fonction de l'heure
     private static void afficherMessageAnglais(int heure) {
         if (heure >= 0 && heure < 18) {
             System.out.println("Good Morning");
