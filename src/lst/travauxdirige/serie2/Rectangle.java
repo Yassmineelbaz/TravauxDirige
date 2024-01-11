c2f02ea53da91d9c016c3daf9a136c126d19a35d
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:07:31 +0100

    Rectangle.java

--- src/lst/travauxdirige/serie2/Rectangle.java
+++ src/lst/travauxdirige/serie2/Rectangle.java
@@ -2,29 +2,24 @@ package lst.travauxdirige.serie2;
 
 public class Rectangle {
 
-        // Attributs de la classe
         private double longueur;
         private double largeur;
 
-        // Constructeur par défaut
         public Rectangle() {
             this.longueur = 0.0;
             this.largeur = 0.0;
         }
 
-        // Constructeur avec longueur et largeur
         public Rectangle(double longueur, double largeur) {
             this.longueur = longueur;
             this.largeur = largeur;
         }
 
-        // Constructeur de copie
         public Rectangle(Rectangle rectangle) {
             this.longueur = rectangle.longueur;
             this.largeur = rectangle.largeur;
         }
 
-        // Accesseurs et mutateurs pour longueur et largeur (positifs)
         public double getLongueur() {
             return longueur;
         }
@@ -49,22 +44,18 @@ public class Rectangle {
             }
         }
 
-        // Méthode pour calculer le périmètre du rectangle
         public double perimetre() {
             return 2 * (longueur + largeur);
         }
 
-        // Méthode pour calculer l'aire du rectangle
         public double aire() {
             return longueur * largeur;
         }
 
-        // Méthode pour vérifier si le rectangle est un carré
         public boolean isCarre() {
             return longueur == largeur;
         }
 
-        // Méthode toString pour afficher une représentation du rectangle
         @Override
         public String toString() {
             return "Rectangle [longueur=" + longueur + ", largeur=" + largeur + "]";
