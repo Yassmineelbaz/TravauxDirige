24a157f954a84412e09c7f0091379567c405d374
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:10:42 +0100

    Affichage.java

--- src/lst/travauxdirige/serie1/Affichage.java
+++ src/lst/travauxdirige/serie1/Affichage.java
@@ -7,11 +7,9 @@ public class Affichage {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            // Demander à l'utilisateur de saisir le nombre de lignes
             System.out.print("Entrez le nombre de lignes pour le triangle : ");
             int nombreLignes = scanner.nextInt();
 
-            // Afficher le triangle numérique
             afficherTriangleNumerique(nombreLignes);
 
             scanner.close();
@@ -23,7 +21,6 @@ public class Affichage {
                 for (int j = 1; j <= i; j++) {
                     System.out.print(j);
                 }
-                // Passer à la ligne suivante après chaque ligne du triangle
                 System.out.println();
 
         }
