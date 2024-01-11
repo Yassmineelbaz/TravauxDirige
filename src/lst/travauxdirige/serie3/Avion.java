2d5abdda65a8655beef954b81c88108b8b0cd7ba
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:31:25 +0100

    Avion.java

--- src/lst/travauxdirige/serie3/Avion.java
+++ src/lst/travauxdirige/serie3/Avion.java
@@ -3,12 +3,12 @@ package lst.travauxdirige.serie3;
 class Avion extends Vehicule {
     private int nbMoteur;
 
-    // Constructeur Avion avec un seul paramètre
+   
     public Avion(int nbP) {
-        super(nbP); // Appel du constructeur de la classe Vehicule
+        super(nbP); 
     }
 
-    // Constructeur Avion avec deux paramètres
+    
     public Avion(int nbP, int nbM) {
         super(nbP);
         this.nbMoteur = nbM;
