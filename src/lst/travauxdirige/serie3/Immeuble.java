cadd91a451770f92c61d444396b7b8aeb41f7d01
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:54:42 +0100

    Immeuble.java

--- src/lst/travauxdirige/serie3/Immeuble.java
+++ src/lst/travauxdirige/serie3/Immeuble.java
@@ -3,7 +3,7 @@ package lst.travauxdirige.serie3;
 class Immeuble extends Batiment {
     private int nbAppart;
 
-    // Constructeurs de la classe Immeuble
+   
     public Immeuble() {
         super();
         this.nbAppart = 0;
@@ -14,7 +14,7 @@ class Immeuble extends Batiment {
         this.nbAppart = nbAppart;
     }
 
-    // Accesseurs et mutateurs pour l'attribut nbAppart
+    
     public int getNbAppart() {
         return nbAppart;
     }
@@ -23,7 +23,7 @@ class Immeuble extends Batiment {
         this.nbAppart = nbAppart;
     }
 
-    // Méthode toString pour afficher une représentation de l'Immeuble
+
     @Override
     public String toString() {
         return super.toString() + ", Immeuble - Nb Appartements : " + nbAppart;
