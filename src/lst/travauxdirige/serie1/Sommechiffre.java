70917728a11516bb5723ecb2e506a84518fccdd6
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:26:36 +0100

    Sommechiffre.java

--- src/lst/travauxdirige/serie1/Sommechiffre.java
+++ src/lst/travauxdirige/serie1/Sommechiffre.java
@@ -7,11 +7,9 @@ public class Sommechiffre {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            // Demander à l'utilisateur de saisir un nombre entier
             System.out.print("Entrez un nombre entier : ");
             int nombre = scanner.nextInt();
 
-            // Calculer et afficher la somme des chiffres du nombre
             int sommeChiffres = calculerSommeChiffres(nombre);
             System.out.println("La somme des chiffres de " + nombre + " est : " + sommeChiffres);
 
@@ -21,7 +19,6 @@ public class Sommechiffre {
         private static int calculerSommeChiffres(int nombre) {
             int somme = 0;
 
-            // Utiliser une boucle pour additionner les chiffres du nombre
             while (nombre != 0) {
                 somme += nombre % 10;  // Ajouter le dernier chiffre
                 nombre /= 10;  // Supprimer le dernier chiffre
