f3e7d0ea237545e49d5283cad6695b88bd72da94
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 13:33:20 +0100

    Batiment.java

--- src/lst/travauxdirige/serie3/Batiment.java
+++ src/lst/travauxdirige/serie3/Batiment.java
@@ -3,7 +3,7 @@ package lst.travauxdirige.serie3;
 class Batiment {
     private String adresse;
 
-    // Constructeurs de la classe Batiment
+    
     public Batiment() {
         this.adresse = "Inconnue";
     }
@@ -12,7 +12,7 @@ class Batiment {
         this.adresse = adresse;
     }
 
-    // Accesseurs et mutateurs pour l'attribut adresse
+   
     public String getAdresse() {
         return adresse;
     }
@@ -21,7 +21,7 @@ class Batiment {
         this.adresse = adresse;
     }
 
-    // Méthode toString pour afficher une représentation du Batiment
+    
     @Override
     public String toString() {
         return "Bâtiment - Adresse : " + adresse;
