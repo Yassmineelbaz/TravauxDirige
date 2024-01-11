2cd09bbf2acee2c419cfe499bdf1487f2895dc33
Yassmineelbaz <156314456+Yassmineelbaz@users.noreply.github.com>
2024-01-11 14:22:30 +0100

    PGCD.java

--- src/lst/travauxdirige/serie1/PGCD.java
+++ src/lst/travauxdirige/serie1/PGCD.java
@@ -6,21 +6,18 @@ public class PGCD {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Demander à l'utilisateur de saisir deux entiers positifs non nuls
         System.out.print("Entrez le premier entier positif non nul (a) : ");
         int a = scanner.nextInt();
 
         System.out.print("Entrez le deuxième entier positif non nul (b) : ");
         int b = scanner.nextInt();
 
-        // Calculer et afficher le PGCD
         int pgcd = calculerPGCD(a, b);
         System.out.println("Le PGCD de " + a + " et " + b + " est : " + pgcd);
 
         scanner.close();
     }
 
-    // Méthode pour calculer le PGCD en utilisant l'algorithme d'Euclide
     private static int calculerPGCD(int a, int b) {
         while (b != 0) {
             int r = a % b;
